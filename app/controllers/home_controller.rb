cb480222f9f158fb6472128de205257b8d87373b
Kender ROMAIN <kenderromain@gmail.com>
2023-03-25 22:36:26 -0400

    Create the User model.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    flash[:notice] = "Logged in successfuly"
-    flash[:alert] = "Invalid email or password"
+    flash.now[:notice] = "Logged in successfuly"
+    flash.now[:alert] = "Invalid email or password"
   end
 end
