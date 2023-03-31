a4e15ffc168dce5243deed259ef29012d8438be9
Kender ROMAIN <kenderromain@gmail.com>
2023-03-31 15:04:16 -0400

    Add validation for email field.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
+# User Model Description
+# email :string
+# password_digest :string (For hashing purpose) will generate these virtual fields :
+# password :string virtual and 
+# pasword_confirmation :string virtual
 class User < ApplicationRecord
   has_secure_password
+  validates :email, presence: true
 end
